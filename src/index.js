869be2ffaecca97d45164612c8a4839a06ce2ea9
devesh0477 <55842034+devesh0477@users.noreply.github.com>
2020-10-24 20:27:12 -0400

    Practice till Challenge 3

--- src/index.js
+++ src/index.js
@@ -2,3 +2,121 @@
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
+
+// var React = require("react");
+// var ReactDOM = require("react-dom");
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// ReactDOM.render(
+//   <div>
+//     <h1> List of Cars </h1>
+//     <ul>
+//       <li> BMW </li>
+//       <li> HUMMER </li>
+//       <li> MUSTANG </li>
+//     </ul>
+//   </div>,
+//   document.getElementById("root")
+// );
+
+// const name = "Devesh";
+// const lName = "Verma";
+// const luckyNumber = 8;
+
+// ReactDOM.render(
+//   <div>
+//     <h1> Hello {name + lName}! </h1>
+//     <p> My luck number is {luckyNumber} </p>
+//   </div>,
+//   document.getElementById("root")
+// );
+
+// -----------------------------------------------------------------------------------
+//                            CHALLANGE: 2
+// -----------------------------------------------------------------------------------
+
+//Create a react app from scratch.
+//It should display 2 paragraph HTML elements.
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
+//E.g.
+//Created by Angela Yu.
+//Copyright 2019.
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// const fName = "Devesh";
+// const lName = "Verma";
+// const year = 2020;
+
+// const currentDate = new Date();
+// const currentYear = currentDate.getFullYear();
+
+// ReactDOM.render(
+//   <div>
+//     <p> Created by {fName + lName} </p>
+//     <p> Copyright {currentYear}.</p>
+//   </div>,
+//   document.getElementById("root")
+// );
+
+// -----------------------------------------------------------------------------------
+//                            CHALLANGE: 3
+// -----------------------------------------------------------------------------------
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// const randomImages = "https://picsum.photos/seed/picsum/200/300";
+
+// ReactDOM.render(
+//   <div>
+//     <h1 className="a"> CAR LIST </h1>
+
+//     <div>
+//       <img
+//         className="imges"
+//         src="https://static-ssl.businessinsider.com/image/5d9b5bff52887931e8497a36-1405/141222twnmustang.jpg"
+//         alt="carImage"
+//       />
+
+//       <img className="imges" src={randomImages} alt="randomImage" />
+
+//       <img
+//         className="imges"
+//         src={randomImages + "?grayscale"}
+//         alt="randomImage"
+//       />
+//     </div>
+//   </div>,
+
+//   document.getElementById("root")
+// );
+
+// -----------------------------------------------------------------------------------
+//                            Practice : challenge:3
+// -----------------------------------------------------------------------------------
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// const imgUrl = "https://picsum.photos/id/237/200/300";
+
+// ReactDOM.render(
+//   <div>
+//     <h1>hello Mylo</h1>
+
+//     <div>
+//       <img className="imges" src={imgUrl} alt="loreumImpsum" />
+//       <img className="imges" src={imgUrl + "?grayscale"} alt="loreumImpsum" />
+//     </div>
+//   </div>,
+//   document.getElementById("root")
+// );
+
+// -----------------------------------------------------------------------------------
+//                            CHALLANGE: 4
+// -----------------------------------------------------------------------------------
